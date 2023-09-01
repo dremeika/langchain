b811fc8b5d2a0ebef8c16cba0845f92924f2b9fa
Donatas Remeika <donatas.remeika@gmail.com>
2023-09-01 19:32:03 +0300

    Working initial implementation

---
+++ libs/langchain/langchain/utilities/searchapi.py
@@ -0,0 +1,123 @@
+from typing import Dict, Optional
+
+import aiohttp
+import requests
+
+from langchain.pydantic_v1 import BaseModel, root_validator
+from langchain.utils import get_from_dict_or_env
+
+
+class SearchApiAPIWrapper(BaseModel):
+    """
+    Wrapper around SearchApi API.
+    """
+
+    # Use "google" engine by default. Full list of supported ones can be found in https://www.searchapi.io/docs/google
+    engine: str = "google"
+    searchapi_api_key: Optional[str] = None
+    aiosession: Optional[aiohttp.ClientSession] = None
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        arbitrary_types_allowed = True
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        """Validate that API key exists in environment."""
+        searchapi_api_key = get_from_dict_or_env(
+            values, "searchapi_api_key", "SEARCHAPI_API_KEY"
+        )
+        values["searchapi_api_key"] = searchapi_api_key
+        return values
+
+    def run(self, query: str) -> str:
+        results = self.results(query)
+        return self._result_as_string(results)
+
+    async def arun(self, query: str) -> str:
+        results = await self.aresults(query)
+        return self._result_as_string(results)
+
+    def results(self, query: str) -> dict:
+        results = self._search_api_results(query)
+        return results
+
+    async def aresults(self, query: str) -> dict:
+        results = await self._async_search_api_results(query)
+        return results
+
+    def _prepare_request(self, query: str) -> dict:
+        return {
+            "url": "https://www.searchapi.io/api/v1/search",
+            "headers": {
+                "Authorization": f"Bearer {self.searchapi_api_key}",
+            },
+            "params": {
+                "engine": self.engine,
+                "q": query,
+            },
+        }
+
+    def _search_api_results(self, query: str) -> dict:
+        request_details = self._prepare_request(query)
+        response = requests.get(
+            url=request_details["url"],
+            params=request_details["params"],
+            headers=request_details["headers"],
+        )
+        # TODO: check if response is 200
+        return response.json()
+
+    async def _async_search_api_results(self, query: str) -> dict:
+        """Use aiohttp to send request to SearchApi API and return results async."""
+        request_details = self._prepare_request(query)
+        if not self.aiosession:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    url=request_details["url"],
+                    headers=request_details["headers"],
+                    params=request_details["params"],
+                ) as response:
+                    results = await response.json()
+        else:
+            async with self.aiosession.get(
+                url=request_details["url"],
+                headers=request_details["headers"],
+                params=request_details["params"],
+            ) as response:
+                results = await response.json()
+        # TODO: check if response is 200
+        return results
+
+    @staticmethod
+    def _result_as_string(result: dict) -> str:
+        toret = "No good search result found"
+        # TODO: use answer_box answer + snippet when available
+        if "knowledge_graph" in result.keys():
+            toret = result["knowledge_graph"]["description"]
+        elif "organic_results" in result.keys():
+            snippets = [
+                r["snippet"] for r in result["organic_results"] if "snippet" in r.keys()
+            ]
+            toret = "\n".join(snippets)
+        elif "jobs" in result.keys():
+            jobs = [
+                r["description"] for r in result["jobs"] if "description" in r.keys()
+            ]
+            toret = "\n".join(jobs)
+        elif "videos" in result.keys():
+            videos = [
+                f"""Title: "{r["title"]}" Link: {r["link"]}"""
+                for r in result["videos"]
+                if "title" in r.keys()
+            ]
+            toret = "\n".join(videos)
+        elif "images" in result.keys():
+            images = [
+                f"""Title: "{r["title"]}" Link: {r["original"]["link"]}"""
+                for r in result["images"]
+                if "original" in r.keys()
+            ]
+            toret = "\n".join(images)
+        return toret
--- libs/langchain/tests/integration_tests/utilities/test_searchapi_api.py
+++ libs/langchain/tests/integration_tests/utilities/test_searchapi_api.py
@@ -1,9 +1,9 @@
 """Integration tests for SearchApi"""
-from langchain.utilities.searchapi_search import SearchAPIWrapper
+from langchain.utilities.searchapi import SearchApiAPIWrapper
 
 
 def test_call() -> None:
     """Test that call gives the correct answer."""
-    search = SearchAPIWrapper()
+    search = SearchApiAPIWrapper()
     output = search.run("What is the capital of Lithuania?")
     assert "Vilnius" in output
